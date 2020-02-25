1604e472215763186d25d9dec38dfbcee1cd6204
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-25 03:04:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@
 def run_guessing_game
   input = gets.chomp
   num = rand(6) + 1
-  if input == 'exit'
+  if input == num.to_s
+    puts "You guessed the correct number!"
+  elsif input.downcase == 'exit'
     puts "Goodbye!"
-  elsif input != num
-    puts "Sorry! The computer guessed #{num}."
   end
   puts "You guessed the correct number!"
 end
